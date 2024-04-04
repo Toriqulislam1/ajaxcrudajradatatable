1eb613b144588d987d033f9f2f76239799e93e04
toriqul <toriqulislam2720@gmail.com>
2024-04-04 09:32:01 +0600

    ajaxcrudAjradataTable

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductAjaxController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('products-ajax-crud', ProductAjaxController::class);
